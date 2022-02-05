788577b45ea9fd2d656b95523d3238e258ac28dd
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2022-02-05 19:38:42 +0800

    chore: get all flowers

--- src/utils/get-mints.ts
+++ src/utils/get-mints.ts
@@ -1,7 +1,5 @@
-import { Connection, PublicKey } from '@solana/web3.js';
+import { Connection } from '@solana/web3.js';
 import { deserializeUnchecked } from 'borsh';
-import jsonFormat from 'json-format';
-import { download } from './download';
 import { METADATA_PROGRAM_ID } from './accounts';
 import { toPublicKey } from './toPublicKey';
 import { Metadata, METADATA_SCHEMA } from './metadata-schema';
@@ -26,14 +24,5 @@ export const getMints = async (creatorId: string, url: string) => {
     ]
   });
   const deserialized = a.map((b) => deserializeUnchecked(METADATA_SCHEMA, Metadata, b.account.data));
-  download(
-    'mints-creatorId-' + Date.now() + '.json',
-    jsonFormat(
-      deserialized.map((g) => new PublicKey(g.mint).toBase58()),
-      {
-        type: 'space',
-        size: 2
-      }
-    )
-  );
+  return deserialized;
 };
