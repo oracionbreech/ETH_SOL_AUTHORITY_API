37f4a117b56aaec1d5a4247e8c40a0074246cdbb
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2022-02-08 20:53:05 +0800

    feat: consumes attributes first

--- src/controllers/flowers/sourceAllFlowers.ts
+++ src/controllers/flowers/sourceAllFlowers.ts
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { Response, Request } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { get, isEmpty } from 'lodash';
 
 // Models
 import Flower, { IFlower } from '../../model/flower';
@@ -7,16 +9,47 @@ import Flower, { IFlower } from '../../model/flower';
 // Utilities
 import { getMints } from '../../utils/get-mints';
 
+const createFlowerObject = async ({ arweaveURI, arweaveData }): Promise<IFlower> => {
+  const attributes = get(arweaveData, 'attributes', []);
+
+  const creators = get(arweaveData, 'creators', []);
+
+  const description = get(arweaveData, 'description', '');
+
+  const image = get(arweaveData, 'image', '');
+
+  const name = get(arweaveData, 'name', '');
+  return {
+    arweaveURI,
+    attributes,
+    creators,
+    description,
+    image,
+    name
+  };
+};
+
 const sourceAllFlowers = async (req: Request, res: Response): Promise<any> => {
-  const connectionURL = 'https://api.metaplex.solana.com';
+  const connectionURL = 'https://bold-red-bird.solana-mainnet.quiknode.pro/37568faa0ee51bbef90e87c3cbd55064d80a5519/';
 
   try {
     const mints = await getMints('8J9W44AfgWFMSwE4iYyZMNCWV9mKqovS5YHiVoKuuA2b', connectionURL);
 
-    const mintedNames: IFlower[] = mints.map((mint) => ({
-      name: String(mint.data.name).replaceAll('\x00', ''),
-      arweaveURI: String(mint.data.uri).replaceAll('\x00', '')
-    }));
+    const mintedNames: IFlower[] = await Promise.all(
+      mints
+        .filter(
+          (mint) =>
+            String(mint.data.uri).replaceAll('\x00', '') !== 'null' &&
+            !isEmpty(String(mint.data.uri).replaceAll('\x00', ''))
+        )
+        .map(async (mint) => {
+          const arweaveURI = String(mint.data.uri).replaceAll('\x00', '');
+
+          const { data } = await axios.get(arweaveURI);
+
+          return createFlowerObject({ arweaveData: data, arweaveURI });
+        })
+    );
 
     const createFlowers = await Flower.insertMany(mintedNames);
 
--- src/model/flower.ts
+++ src/model/flower.ts
@@ -1,13 +1,31 @@
 import mongoose, { Schema } from 'mongoose';
 
+export interface IFlowerAttributes {
+  traitType: string;
+  value: string;
+}
+
+export interface IFlowerCreators {
+  address: string;
+  share: number;
+}
+
 export interface IFlower {
   name: string;
   arweaveURI: string;
+  description: string;
+  image: string;
+  attributes: IFlowerAttributes[];
+  creators: IFlowerCreators[];
 }
 
 const Flower = new Schema<IFlower>({
   name: String,
-  arweaveURI: String
+  arweaveURI: String,
+  description: String,
+  image: String,
+  attributes: Schema.Types.Array,
+  creators: Schema.Types.Array
 });
 
 Flower.set('timestamps', true);
