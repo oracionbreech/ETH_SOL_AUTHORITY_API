5f79139b4669d82b56b76c3f5dcf91e1e947a699
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2022-02-18 19:41:32 +0800

    chore: remove discord

--- src/controllers/discord/requestCode.ts
+++ src/controllers/discord/requestCode.ts
@@ -4,7 +4,7 @@ import { StatusCodes } from 'http-status-codes';
 const requestCode = async (req: Request, res: Response): Promise<any> => {
   try {
     return res.status(StatusCodes.OK).json({
-      url: 'https://discord.com/oauth2/authorize?client_id=943755436207796224&scope=identify&response_type=code&redirect_uri=http://localhost:3000/discord',
+      url: 'https://discord.com/api/oauth2/authorize?client_id=943755436207796224&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord&response_type=code&scope=identify%20guilds',
       type: 'auth_url'
     });
   } catch (error) {
