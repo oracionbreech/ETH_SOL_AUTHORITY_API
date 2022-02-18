9633fc87d57c3cf5de66009fd3ecec91f702aa95
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2022-02-18 19:58:49 +0800

    feat: get members with access token

--- src/controllers/discord/getMembers.ts
+++ src/controllers/discord/getMembers.ts
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import { Response, Request } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-const getMembers = async (req: Request, res: Response): Promise<any> => {
+interface GetMemberInfoRequest extends Request {
+  query: {
+    token: string;
+  };
+}
+
+const getMembers = async (req: GetMemberInfoRequest, res: Response): Promise<any> => {
   try {
-    return res.status(StatusCodes.OK).json([]);
+    const guildMemberInfo = await axios.get('https://discordapp.com/api/users/@me/guilds/894275198231121960/member', {
+      headers: {
+        Authorization: `Bearer ${req.query.token}`
+      }
+    });
+
+    return res.status(StatusCodes.OK).json(guildMemberInfo.data);
   } catch (error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
   }
--- src/routes/discord.ts
+++ src/routes/discord.ts
@@ -8,6 +8,6 @@ import requestCode from '../controllers/discord/requestCode';
 import requestTokenGrant from '../controllers/discord/requestTokenGrant';
 import getMembers from '../controllers/discord/getMembers';
 
-const DiscordRoutes = router.get('/', requestCode).post('/token', requestTokenGrant).get('/members', getMembers);
+const DiscordRoutes = router.get('/', requestCode).get('/roles', requestTokenGrant).get('/members', getMembers);
 
 export default DiscordRoutes;
