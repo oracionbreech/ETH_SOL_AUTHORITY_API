95f7112042d32db60287c5064a95b1b0abd15f8d
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2022-03-03 21:40:26 +0800

    chore: added a v2 get members

--- src/routes/discord.ts
+++ src/routes/discord.ts
@@ -7,7 +7,12 @@ const router = Router();
 import requestCode from '../controllers/discord/requestCode';
 import requestTokenGrant from '../controllers/discord/requestTokenGrant';
 import getMembers from '../controllers/discord/getMembers';
+import getMembersV2 from '../controllers/discord/getMembersV2';
 
-const DiscordRoutes = router.get('/', requestCode).get('/roles', requestTokenGrant).get('/members', getMembers);
+const DiscordRoutes = router
+  .get('/', requestCode)
+  .get('/roles', requestTokenGrant)
+  .get('/members', getMembers)
+  .get('/v2/members', getMembersV2);
 
 export default DiscordRoutes;
