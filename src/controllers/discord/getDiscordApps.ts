8a08289224288a2240504c2c05942337f1920ace
oracionbreech <oracionbre@gmail.com>
2022-03-11 22:14:41 +0800

    chore: added get discord apps controller

---
+++ src/controllers/discord/getDiscordApps.ts
@@ -0,0 +1,34 @@
+import { Response, Request } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { isEmpty } from 'lodash';
+
+// Models
+import DiscordApp from '../../model/DiscordApp';
+
+interface AddDiscordAppRequest extends Request {
+  query: {
+    userId: string;
+  };
+}
+
+const getDiscordApps = async (req: AddDiscordAppRequest, res: Response): Promise<any> => {
+  try {
+    const { userId } = req.query;
+
+    if (isEmpty(userId)) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: 'Invalid query.'
+      });
+    }
+
+    const newDiscordApp = await DiscordApp.create({
+      ...req.body
+    });
+
+    return res.status(StatusCodes.OK).json(newDiscordApp);
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
+
+export default getDiscordApps;
