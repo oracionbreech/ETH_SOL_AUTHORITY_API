18125d3b7b96d213aa0a6fd4f7a47ff2409fe32a
oracionbreech <oracionbre@gmail.com>
2022-03-12 13:13:16 +0800

    chore: added signup route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,6 +6,6 @@ const router = Router();
 // controllers
 import signUp from '../controllers/users/signUp';
 
-const UserRoutes = router.post('/', signUp);
+const UserRoutes = router.post('/sign-up', signUp);
 
 export default UserRoutes;
