57e40b50669f24967c60532bbbac056693632e8c
oracionbreech <oracionbre@gmail.com>
2022-03-15 14:39:28 +0800

    chore: find discord app before requesting grant

--- src/controllers/discord/requestTokenGrant.ts
+++ src/controllers/discord/requestTokenGrant.ts
@@ -2,32 +2,46 @@ import axios from 'axios';
 import { Response, Request } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import qs from 'qs';
+import DiscordApp from '../../model/DiscordApp';
 
 interface RequestTokenGrant extends Request {
   query: {
     code: string;
+    clientId: string;
   };
 }
 
 const requestTokenGrant = async (req: RequestTokenGrant, res: Response): Promise<any> => {
   try {
-    const { code } = req.query;
+    const { code, clientId } = req.query;
+
+    if (!code || !clientId)
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: 'Invalid code or client ID.'
+      });
+
+    const findDiscordApp = await DiscordApp.findOne({
+      clientId
+    });
+
+    if (!findDiscordApp)
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: `Can't find discord app requested.`
+      });
 
     const data = qs.stringify({
       code,
       grant_type: 'authorization_code',
       redirect_uri: String(process.env.DISCORD_AUTH_REDIRECT_URI),
-      client_secret: String(process.env.DISCORD_CLIENT_SECRET),
-      client_id: String(process.env.DISCORD_CLIENT_ID)
+      client_secret: String(findDiscordApp.secretKey),
+      client_id: String(findDiscordApp.clientId)
     });
 
     const response = await axios({
       method: 'post',
       url: 'https://discordapp.com/api/oauth2/token',
       headers: {
-        Authorization: `Basic ${btoa(
-          `${String(process.env.DISCORD_CLIENT_ID)}:${String(process.env.DISCORD_CLIENT_SECRET).toString()}`
-        )}`,
+        Authorization: `Basic ${btoa(`${String(clientId)}:${String(process.env.DISCORD_CLIENT_SECRET).toString()}`)}`,
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       data: data
