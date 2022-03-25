b0b5e3b76d932e29edf56f6d6ba8fba60de10c0c
oracionbreech <oracionbre@gmail.com>
2022-03-25 14:11:03 +0800

    chore: api base url from env

--- src/services/twitter.ts
+++ src/services/twitter.ts
@@ -4,5 +4,5 @@ import TwitterLogin from 'twitter-login';
 export const twitter = new TwitterLogin({
   consumerKey: process.env.TWITTER_API_KEY,
   consumerSecret: process.env.TWITTER_API_SECRET,
-  callbackUrl: 'https://402c-180-191-117-39.ngrok.io/twitter/auth/userToken'
+  callbackUrl: `${process.env.API_BASE_URL}/twitter/auth/userToken`
 });
