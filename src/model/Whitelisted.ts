440dda6c97b6b529da07162d43e6ea6a414bfcca
oracionbreech <oracionbre@gmail.com>
2022-04-11 19:10:45 +0800

    chore: added ip array in whitelisted

--- src/model/Whitelisted.ts
+++ src/model/Whitelisted.ts
@@ -1,15 +1,17 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema, Types } from 'mongoose';
 
 export interface IWhitelisted {
   metamask: string;
   solana: string;
   twitter: string;
+  ipsUsed: [];
 }
 
 const Whitelisted = new Schema<IWhitelisted>({
   metamask: String,
   solana: String,
-  twitter: String
+  twitter: String,
+  ipsUsed: Types.Array
 });
 
 Whitelisted.set('timestamps', true);
