b58738ce39dbb78c450e7823ad6aa78b342e57a5
oracionbreech <oracionbre@gmail.com>
2022-07-02 16:10:11 +0800

    feat: get auction by id

---
+++ src/controllers/auction/getAuctionByID.ts
@@ -0,0 +1,29 @@
+import { Response, Request } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+// Models
+import Auction from '../../model/Auction';
+
+const getAuctionByID = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const { id } = req.params;
+
+    if (!id)
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: 'Please specify id'
+      });
+
+    const auction = await Auction.findOne({});
+
+    if (!auction)
+      return res.status(StatusCodes.NOT_FOUND).json({
+        message: 'No auction found by ID.'
+      });
+
+    return res.status(StatusCodes.OK).json(auction);
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
+
+export default getAuctionByID;
--- src/routes/auction.ts
+++ src/routes/auction.ts
@@ -4,11 +4,12 @@ import { config } from 'dotenv';
 // Controllers
 import getAuctions from '../controllers/auction/getAuctions';
 import registerAuctionInfo from '../controllers/auction/registerAuctionInfo';
+import getAuctionByID from '../controllers/auction/getAuctionByID';
 
 config();
 const router = Router();
 
 // controllers
-const AuctionRoutes = router.get('/', getAuctions).post('/', registerAuctionInfo);
+const AuctionRoutes = router.get('/', getAuctions).post('/', registerAuctionInfo).get('/:id', getAuctionByID);
 
 export default AuctionRoutes;
